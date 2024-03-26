4e848953fbb43819f00b434366ca17776451044e
muhammad alif alferdiansyah <alifalferdiansyah7@gmail.com>
2024-03-26 10:55:32 +0700

    sudah bisa ini

--- model/connection.php
+++ model/connection.php
@@ -3,3 +3,20 @@ $conn = mysqli_connect('localhost', 'root', '', 'mahasiswa');
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
 ?>
+<?php
+class Connection {
+    private $conn;
+
+    public function __construct() {
+        $this->conn = mysqli_connect('localhost', 'root', '', 'mahasiswa');
+
+        if ($this->conn->connect_error) {
+            die("Connection failed: " . $this->conn->connect_error);
+        }
+    }
+
+    public function getConnection() {
+        return $this->conn;
+    }
+}
+?>
--- model/model.php
+++ model/model.php
@@ -9,7 +9,7 @@ class Model {
         $this->conn = $connection->getConnection();
     }
 
-    public function validateCredentials($nim) {
+    public function checknim($nim) {
         $stmt = $this->conn->prepare("SELECT nim FROM mahasiswa WHERE nim = ?");
         $stmt->bind_param("s", $nim);
         $stmt->execute();
@@ -30,6 +30,35 @@ class Model {
         }
 
         return $kelas;
+    }
+    public function registerUser($nim, $password) {
+        $nim = mysqli_real_escape_string($this->conn, $nim);
+        $password = mysqli_real_escape_string($this->conn, $password);
+
+        $check_query = "SELECT * FROM nim WHERE nim='$nim'";
+        $check_akunlain = "SELECT * FROM user WHERE nim='$nim'";
+        $result = mysqli_query($this->conn, $check_query);
+        $result_check_akun = mysqli_query($this->conn, $check_akunlain);
+
+        if (!$result) {
+            return "Error: " . mysqli_error($this->conn);
+        }
+
+        $num_rows = mysqli_num_rows($result);
+        $num_rows_akun = mysqli_num_rows($result_check_akun);
+        if ($num_rows == 0) {
+            return "nim tidak dalam database";
+        }
+        if ($num_rows_akun > 0) {
+            return "nim sudah terdaftar";
+        }
+
+        $insert_query = "INSERT INTO user (nim, password) VALUES ('$nim', '$password')";
+        if (mysqli_query($this->conn, $insert_query)) {
+            return "berhasil registrasi";
+        } else {
+            return "Error: " . $insert_query . "<br>" . mysqli_error($this->conn);
+        }
     }
 }
 ?>
